27ead76307625d5ea605267584bff243f03b71ad
Leidi <leididelapuente3@gmail.com>
2024-03-17 23:14:34 -0500

    Add Log In testing Set Up

---
+++ client/src/pages/shared/LogIn/LogIn.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+//component
+import { LogIn } from './LogIn';
+
+/**
+ * TODO: Envio del formulario, navegacion hacia el inicio, navegacion hacia el form registrar, manejo de errores de validacion, manejo de errores del servidor, guardar en local storage
+ */
+
+describe('Log In Page', () => {
+	beforeEach(() => render(<LogIn />));
+	test('Should render log in form', () => {
+		const loginForm = screen.getByRole('form');
+		expect(loginForm).toBeInTheDocument();
+	});
+	test('Should catch the changes in the inputs', async () => {
+		const codeInput = screen.getByLabelText('Codigo');
+		const passwordInput = screen.getByLabelText('Contraseña');
+
+		expect(codeInput).toBeInTheDocument();
+		expect(passwordInput).toBeInTheDocument();
+
+		user.type(codeInput, '123456789');
+		user.type(passwordInput, '1234567');
+
+		await waitFor(() => {
+			expect(screen.getByDisplayValue('123456789')).toBeInTheDocument();
+			expect(screen.getByDisplayValue('1234567')).toBeInTheDocument();
+		});
+	});
+});
