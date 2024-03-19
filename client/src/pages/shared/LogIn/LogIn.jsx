a12583a68effee4de33a41158990d372a976a925
Leidi <leididelapuente3@gmail.com>
2024-03-19 16:55:02 -0500

    Component (LogIn):  Refactor Form's Styles by Adding Mixins

--- client/src/pages/shared/LogIn/LogIn.jsx
+++ client/src/pages/shared/LogIn/LogIn.jsx
@@ -18,7 +18,7 @@ export const LogIn = () => {
 	// const queryClient = useQueryClient();
 
 	// const {data: user, isLoading} = useQuery({
-	// 	queryFn: ()=>fetchUsers(), 
+	// 	queryFn: ()=>fetchUsers(),
 	// 	queryKey: ["users"]
 	// });
 
@@ -56,7 +56,7 @@ export const LogIn = () => {
 		// 		} else if (userRol === 3) {
 		// 			navigate('/indexProfessors');
 		// 		}
-				
+
 		// 	} else {
 		// 		console.error('Error al iniciar sesion', response.data.error);
 		// 	}
@@ -65,79 +65,76 @@ export const LogIn = () => {
 		// 	console.error('Error: ', error.message);
 		// }
 		reset();
-  
 	};
 	return (
 		<>
-		{/* {isError === true && <PopUpError message={error}/>} */}
-		<main className={styles.main}>
-			<div className={styles.container}>
-				<h2 className={styles.container__title}>¡Bienvenido otra vez!</h2>
-				<p className={styles.container__text}>
-					Estamos encantados de tenerte de regreso. Por favor, inicia sesión
-					para acceder a tu cuenta y continuar explorando todo lo que tenemos
-					para ofrecerte.
-				</p>
-			</div>
-			<form
-				className={styles.form}
-				role='form'
-				onSubmit={handleSubmit(handleLogIn)}
-			>
-				<h3 className={styles.form__title}>Inicia Sesion</h3>
-				<label htmlFor='email' className={styles.form__label}>
-					Email
-				</label>
-				<input
-					type='email'
-					id='email'
-					className={styles.form__input}
-					{...register('email', {
-						required: {
-							value: true,
-							message: 'El email es requerido',
-						},
-						pattern: {
-							value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
-							message: 'por favor, ingresar un email valido'
-						}
-					})}
-				/>
-				{errors.email && (
-					<ValidationError message={errors.email.message} />
-				)}
-				<label htmlFor='password' className={styles.form__label}>
-					Contraseña
-				</label>
-				<input
-					type='password'
-					id='password'
-					className={styles.form__input}
-					{...register('password', {
-						required: {
-							value: true,
-							message: 'La contraseña es requerida',
-						}
-        	})}
-				/>
-				{errors.password && (
-					<ValidationError message={errors.password.message} />
-				)}
-				{/* {data.code === 401 && <ValidationError message="Correo o contraseña incorrectos" />} */}
-				<a className={styles.form__link}>¿Olvidaste tu contraseña?</a>
-				<input
-					type='submit'
-					value='Iniciar Sesion'
-					className={styles.form__button}
-				/>
-				<p>
-					¿No tienes cuenta?
-					<Link className={styles.form__linkBlue} to='/register'>
-						Registrate
-					</Link>
-				</p>
-			</form>
-		</main>
+			{/* {isError === true && <PopUpError message={error}/>} */}
+			<main className={styles.main}>
+				<div className={styles.container}>
+					<h2 className={styles.container__title}>¡Bienvenido otra vez!</h2>
+					<p className={styles.container__text}>
+						Estamos encantados de tenerte de regreso. Por favor, inicia sesión
+						para acceder a tu cuenta y continuar explorando todo lo que tenemos
+						para ofrecerte.
+					</p>
+				</div>
+				<form
+					className={styles.form}
+					role='form'
+					onSubmit={handleSubmit(handleLogIn)}
+				>
+					<h3 className={styles.form__title}>Inicia Sesion</h3>
+					<label htmlFor='email' className={styles.form__label}>
+						Email
+					</label>
+					<input
+						type='email'
+						id='email'
+						className={styles.form__input}
+						{...register('email', {
+							required: {
+								value: true,
+								message: 'El email es requerido',
+							},
+							pattern: {
+								value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+								message: 'por favor, ingresar un email valido',
+							},
+						})}
+					/>
+					{errors.email && <ValidationError message={errors.email.message} />}
+					<label htmlFor='password' className={styles.form__label}>
+						Contraseña
+					</label>
+					<input
+						type='password'
+						id='password'
+						className={styles.form__input}
+						{...register('password', {
+							required: {
+								value: true,
+								message: 'La contraseña es requerida',
+							},
+						})}
+					/>
+					{errors.password && (
+						<ValidationError message={errors.password.message} />
+					)}
+					{/* {data.code === 401 && <ValidationError message="Correo o contraseña incorrectos" />} */}
+					<a className={styles.form__link}>¿Olvidaste tu contraseña?</a>
+					<input
+						type='submit'
+						value='Iniciar Sesion'
+						className={styles.form__button}
+					/>
+					<p>
+						¿No tienes cuenta?{' '}
+						<Link className={styles.form__linkBlue} to='/register'>
+							Registrate
+						</Link>
+					</p>
+				</form>
+			</main>
 		</>
 	);
 };
