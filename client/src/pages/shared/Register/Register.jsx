8322681078d39d537505eed8e420a3a3195e5aaa
Leidi <leididelapuente3@gmail.com>
2024-03-19 16:55:23 -0500

    Add Register Component

--- client/src/pages/shared/Register/Register.jsx
+++ client/src/pages/shared/Register/Register.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom"
+import styles from './Register.module.scss';
+
 export const Register = () => {
   return (
-    <div>Register</div>
+    <main className={styles.main}>
+      <form method="post" className={styles.form} role="form">
+        <h3 className={styles.form__title}>Crea una cuenta</h3>
+        <input
+          type="text"
+          name="codigo"
+          placeholder="Codigo"
+          className={styles.form__input}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          className={styles.form__input}
+        />
+        
+        <input type="submit" value="Registrarse" className={styles.form__button}/>
+        <p>
+          ¿Ya tienes una cuenta? <Link to="/" className={styles.form__link}>Inicia sesion</Link>
+        </p>
+      </form>
+    </main>
   )
 }
