58f5c9e185e8669258afbeaffea4bb81541e3816
Plataforma Proyectos De Aula <102835770+PlataformaProyectosAula@users.noreply.github.com>
2024-03-28 23:02:19 -0500

    style(nav.jsx, nav.module.scss): add Student Nav Component

---
+++ client/src/components/layout/nav/StudentNav/Nav.jsx
@@ -0,0 +1,61 @@
+// Styles
+import styles from '../Nav.module.scss';
+// Icons
+import { FaHouseChimney, FaMagnifyingGlass } from 'react-icons/fa6';
+import { FaBell } from 'react-icons/fa';
+import { IoLogOut } from 'react-icons/io5';
+import { CgProfile } from 'react-icons/cg';
+// Dependencies
+import { Link } from 'react-router-dom';
+import icon from '../../../../assets/img/default/icon.png';
+import { useEffect, useState } from 'react';
+
+export const Nav = () => {
+
+    const [nav, setNav] = useState(false);
+
+	return (
+		<nav className={styles.nav}>
+			<div className={styles.nav__imgContainer}>
+				<img src={icon} alt='user icon' className={styles.nav__img} />
+			</div>
+			<div className={styles.nav__buttonContainer}>
+				<button
+					type='button'
+					role='button'
+					className={styles.nav__button}
+				>
+				<FaBell />
+
+                </button>
+				<button type='button' role='button' className={styles.nav__buttonNav} onClick={()=>setNav(!nav)}>
+					<span className={styles.nav__span}></span>
+                    <span className={styles.nav__span}></span>
+                    <span className={styles.nav__span}></span>
+				</button>
+			</div>
+			<ul role='nav' className={nav ? styles.nav__list : styles.nav__listHidden}>
+				<li>
+					<Link to='/IndexStudent'>
+						<FaHouseChimney /> Inicio
+					</Link>
+				</li>
+				<li>
+					<Link to='/buscador'>
+						<FaMagnifyingGlass /> Buscar
+					</Link>
+				</li>
+				<li>
+					<Link to='/perfil'>
+						<CgProfile /> Mi Pefil
+					</Link>
+				</li>
+				<li>
+					<button type='button' role='button' className={styles.nav__buttonLogOut}>
+						<IoLogOut /> Cerrar Sesion
+					</button>
+				</li>
+			</ul>
+		</nav>
+	);
+};
