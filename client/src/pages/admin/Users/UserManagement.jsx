989e42c572e173b549cbe083fa68a2c8c61b4e1d
Leidi <leididelapuente3@gmail.com>
2024-04-05 08:27:45 -0500

    Remove Stuents Nav from user management page

--- client/src/pages/admin/Users/UserManagement.jsx
+++ client/src/pages/admin/Users/UserManagement.jsx
@@ -2,7 +2,6 @@
 import { useContext } from 'react';
 // Components
 import { Nav } from '../../../components/layout/nav/AdminNav/Nav';
-import { Nav as StudentNav} from '../../../components/layout/nav/StudentNav/Nav';
 import { UsersList } from '../../../components/users/usersList/UsersList';
 import { ConfirmationPopUp } from '../../../components/utils/confirmation/ConfirmationPopUp';
 // Context
@@ -14,8 +13,7 @@ export const UserManagement = () => {
 
 	return (
 		<>
-			{/* <Nav /> */}
-			<StudentNav/>
+			<Nav />
 			{visible.deactivateUserWarning && <ConfirmationPopUp message="¿Esta seguro de querer desactvar el perfil?" visible={visible} setVisible={setVisible} />}
 			{visible.listUsersError && <ErrorPopUp/>}
 			<main>
