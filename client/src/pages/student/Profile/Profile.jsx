521572e48c6623a77855d35c666ec418ccd6d40a
Leidi <leididelapuente3@gmail.com>
2024-04-23 16:25:29 -0500

    feat(profile.jsx): create profile component

--- client/src/pages/student/Profile/Profile.jsx
+++ client/src/pages/student/Profile/Profile.jsx
@@ -15,16 +15,27 @@ export const Profile = () => {
 		<>
 			<main>
 				<Nav />
-				<section>
-					<button>
+				<section className={styles.profile}>
+					<button className={styles.profile__button}>
 						<IoSettingsSharp />
 					</button>
-					<img src={icon} alt='profile picture' />
-					<p>User name</p>
-					<p>Description</p>
+					<div className={styles.section__img__container}>
+						<img
+							src={icon}
+							alt='profile picture'
+							className={styles.profile__img}
+						/>
+					</div>
 					<div>
-						<div>Carrera</div>
-						<div>Semestre</div>
+						<p className={styles.profile__text__bold}>@user_name</p>
+						<p className={styles.profile__text__light}>nombre y apellido</p>
+					</div>
+					<p className={styles.profile__text__regular}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, facere?
+					</p>
+					<div className={styles.profile__container}>
+						<div className={styles.profile__rectangle}>Carrera</div>
+						<div className={styles.profile__rectangle}>Semestre</div>
 					</div>
 				</section>
 			</main>
