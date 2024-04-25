0e0d87063521c7a727dc0dc0c67a73b6474cad32
Leidi <leididelapuente3@gmail.com>
2024-04-25 16:34:44 -0500

    feat(indexstudent.jsx): create component to displat trending projects in the index of the students

--- client/src/pages/student/Start/IndexStudent.jsx
+++ client/src/pages/student/Start/IndexStudent.jsx
@@ -1,25 +1,104 @@
 //Styles
-
+import styles from './IndexStudent.module.scss';
 // Dependencies
 
 // Request
 
 // Components
 import { Nav } from '../../../components/layout/nav/StudentNav/Nav';
+import { ProjectCard } from '../../../components/project/projectcard/ProjectCard';
+import Number from '../../../components/project/trendnumber/Number';
+import { Link } from 'react-router-dom';
 
 export const IndexStudent = () => {
+	const projects = [
+		{
+			id: 1,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 2,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 3,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 4,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 5,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 6,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 7,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+		{
+			id: 8,
+			title: 'Lorem, ipsum dolor.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fugiat libero eius esse officiis ipsum. FLor...ver mas',
+			likes: 40,
+			comments: 50,
+		},
+	];
+
+	const trend = projects.slice(0, 3);
+
 	return (
 		<>
-			<main>
+			<main className={styles.main}>
 				<Nav />
-				<section>
-					<h3>Anuncios</h3>
+				<section className={styles.section}>
+					<h3 className={styles.section__title}>Anuncios</h3>
 				</section>
-				<section>
-					<h3>Tendencias</h3>
+				<section className={styles.section}>
+					<h3 className={styles.section__title}>Tendencias</h3>
+					{trend.map((project) => (
+						<div>
+							<Number trendNumber={trend.indexOf(project) + 1} />
+							<ProjectCard project={project} key={project.id} />
+						</div>
+					))}
+					<Link className={styles.section__link}>Ver mas...</Link>
 				</section>
-				<section>
-					<h3>Para ti</h3>
+				<section className={styles.section}>
+					<h3 className={styles.section__title}>Para ti</h3>
 				</section>
 			</main>
 		</>
